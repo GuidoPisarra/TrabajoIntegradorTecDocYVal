3940dec66fe97a3ec46a00a444b49be22c3964e0
pisarraguido <pisarraguido@gmail.com>
2022-11-16 20:35:40 -0300

    primeros dos puntos

---
+++ Selenium2022/src/UnicenMain.java
@@ -0,0 +1,8 @@
+
+public class UnicenMain {
+	public static void main(String[] args) {
+		UnicenEnglishVersion unicen = new UnicenEnglishVersion();
+		unicen.launchTest();
+		unicen.closeDriver();
+	}
+}
