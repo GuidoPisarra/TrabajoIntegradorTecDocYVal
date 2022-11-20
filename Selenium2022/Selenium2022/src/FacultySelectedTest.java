4fae37134137755f11cad1af92f86d2a400d388e
cattsas <cattsas1980@gmail.com>
2022-11-20 16:00:03 -0300

    Se agrega Assert de verificación en bùsqueda

--- Selenium2022/Selenium2022/src/FacultySelectedTest.java
+++ Selenium2022/Selenium2022/src/FacultySelectedTest.java
@@ -24,6 +24,7 @@ public class FacultySelectedTest {
 
 	public void launchTest() {
 		driver.navigate().to("https://www.unicen.edu.ar");
+		driver.manage().window().maximize();
 		String titulo = driver.getTitle();	
 		String textoEsperado = "Facultad de Ciencias Económicas";
 		
--- Selenium2022/Selenium2022/src/UnicenSearch.java
+++ Selenium2022/Selenium2022/src/UnicenSearch.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
 public class UnicenSearch{
 	private final WebDriver driver;
@@ -15,7 +16,9 @@ public class UnicenSearch{
 
 	public void launchTest() {
 		driver.navigate().to("https://www.unicen.edu.ar");
+		driver.manage().window().maximize();
 		String titulo = driver.getTitle();	
+		String buscado="alumnos";
 		System.out.println("Ingresó en la web " + titulo);
 		
 		// Se espera a que cargue la barra de busqueda
@@ -23,16 +26,18 @@ public class UnicenSearch{
 		WebElement element = (WebElement) wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("search_block_form")));
 		
 		//Buscamos alumnos
-		element.sendKeys("alumnos");
+		element.sendKeys(buscado);
 		
 		//Por ultimo hacemos click en la "lupa" para buscar
 		element = (WebElement) wait.until(ExpectedConditions.visibilityOfElementLocated(By.name("op")));
 		element.click(); 
+		String URL = driver.getCurrentUrl();
+		Assert.assertTrue(URL.toString().contains(buscado),"No buscó lo solicitado");
 	}
 		
 	public void closeDriver() {
 		if(driver!=null) {
-			driver.close();
+		//	driver.close();
 			
 			System.out.println("Finalizo correctamente");
 		}
